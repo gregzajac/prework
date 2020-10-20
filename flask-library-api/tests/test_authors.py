fd0cb12ee1a394736410eb69a49e952a07bb3a3d
Grzegorz Zając <gzajac11@gmail.com>
2020-10-20 14:13:43 +0200

    Added tests for function get_author

--- flask-library-api/tests/test_authors.py
+++ flask-library-api/tests/test_authors.py
@@ -55,3 +55,27 @@ def test_get_authors_with_params(client, sample_data):
         {"first_name": "Alice"},
         {"first_name": "Dan"}
     ]
+
+
+def test_get_single_author(client, sample_data):
+    response = client.get('/api/v1/authors/9')
+    response_data = response.get_json()
+
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is True
+    assert response_data['data']['first_name'] == 'Andrzej'
+    assert response_data['data']['last_name'] == 'Sapkowski'
+    assert response_data['data']['birth_date'] == '21-06-1948'
+    assert len(response_data['data']['books']) == 1
+
+
+def test_get_single_author_not_found(client, sample_data):
+    response = client.get('/api/v1/authors/90')
+    response_data = response.get_json()
+
+    assert response.status_code == 404
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert 'data' not in response_data
+
