a564d6b078d329e4a553b2e1bab3df501bb735d6
Grzegorz Zając <gzajac11@gmail.com>
2020-10-25 23:12:51 +0100

    Added serialization, validation and HTTP requests for Landlord source

--- flask-myrent-api/myrent_app/landlords.py
+++ flask-myrent-api/myrent_app/landlords.py
@@ -1,25 +1,37 @@
-from flask import jsonify
-from myrent_app import app
+from flask import jsonify, abort
+from webargs.flaskparser import use_args
+from myrent_app import app, db
+from myrent_app.models import Landlord, LandlordSchema, landlord_schema, landlord_update_password_schema
 
 
 @app.route('/api/v1/landlords', methods=['GET'])
 def get_all_landlords():
+    data = Landlord.query.all()
+    landlord_schema = LandlordSchema(many=True)
+
     return jsonify({
         'success': True,
-        'data': 'all users'
+        'data': landlord_schema.dump(data)
     })
 
 
 @app.route('/api/v1/landlords/<int:landlord_id>', methods=['GET'])
 def get_one_landlord(landlord_id: int):
+    landlord = Landlord.query.get_or_404(landlord_id, description=f'Landlord with id {landlord_id} not found')
+
     return jsonify({
         'success': True,
-        'data': f'one user with id {landlord_id}'
+        'data': landlord_schema.dump(landlord)
     })
 
 
 @app.route('/api/v1/landlords', methods=['POST'])
-def create_landlord():
+@use_args(landlord_schema)
+def create_landlord(args: dict):
+    landlord = Landlord(**args)
+    db.session.add(landlord)
+    db.session.commit()
+
     return jsonify({
         'success': True,
         'data': f'create new landlord'
@@ -27,7 +39,16 @@ def create_landlord():
 
 
 @app.route('/api/v1/landlords/<int:landlord_id>', methods=['PUT'])
-def update_landlord_password(landlord_id: int):
+@use_args(landlord_update_password_schema)
+def update_landlord_password(args: dict, landlord_id: int):
+    landlord = Landlord.query.get_or_404(landlord_id, description=f'Landlord with id {landlord_id} not found')
+
+    if not landlord.is_password_valid(args['current_password']):
+        abort(401, description='Invalid password')
+
+    landlord.password = args['new_password']
+    db.session.commit()
+    
     return jsonify({
         'success': True,
         'data': f'update user with id {landlord_id}'
@@ -36,7 +57,12 @@ def update_landlord_password(landlord_id: int):
 
 @app.route('/api/v1/landlords/<int:landlord_id>', methods=['DELETE'])
 def delete_landlord(landlord_id: int):
+    landlord = Landlord.query.get_or_404(landlord_id, description=f'Landlord with id {landlord_id} not found')
+    db.session.delete(landlord)
+    db.session.commit()
+
     return jsonify({
         'success': True,
         'data': f'delete user with id {landlord_id}'
     })
+
--- flask-myrent-api/myrent_app/models.py
+++ flask-myrent-api/myrent_app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from marshmallow import Schema, fields, validate
 from myrent_app import db
 
 
@@ -21,3 +22,29 @@ class Landlord(TimestampMixin, db.Model):
 
     def __repr__(self):
         return f'<Landlord>: {self.first_name} {self.last_name}'
+
+    def is_password_valid(self, password: str) -> bool:
+        return self.password == password
+
+
+class LandlordSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    identifier = fields.String(required=True, validate=validate.Length(max=50))
+    email = fields.String(required=True, validate=validate.Length(max=255))
+    first_name = fields.String(required=True, validate=validate.Length(max=100))
+    last_name = fields.String(required=True, validate=validate.Length(max=100))
+    phone = fields.String(required=True, validate=validate.Length(max=50))
+    address = fields.String(required=True, validate=validate.Length(max=255))
+    description = fields.String()
+    password = fields.String(load_only=True, required=True, validate=validate.Length(min=6, max=255))
+    created = fields.DateTime(dump_only=True)
+    updated = fields.DateTime(dump_only=True)
+
+
+class LandlordUpdatePasswordSchema(Schema):
+    current_password = fields.String(required=True, load_only=True, validate=validate.Length(min=6, max=255))
+    new_password = fields.String(required=True, load_only=True, validate=validate.Length(min=6, max=255))
+
+
+landlord_schema = LandlordSchema()
+landlord_update_password_schema = LandlordUpdatePasswordSchema()
